7094304aa29d80b4cff96f1cc73c1c8240a632b6
Mike Mundy <mundy.michael@mayo.edu>
2017-01-26 16:38:56 -0600

    Initial commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+cobra-babel
+===========
+
+Metabolic models are available from many sources but each source uses a different ID namespace. That makes
+combining and comparing models from different sources difficult. cobra-babel provides functions to translate
+between ID namespaces and convert metabolic models to a common ID namespace. cobra-babel supports the
+following namespaces:
+
+1. `ModelSEED <http://modelseed.org>`_
+2. `Virtual Metabolic Human <http://vmh.uni.lu/>`_ (VMH)
+3. `Biochemical, Genetic and Genomic knowledge base <http://bigg.ucsd.edu/>`_ (BiGG)
+
+Features include:
+
+1. Take a list of models, open all of them, create a universal model with all unique reactions and metabolites.
+2. Download specific files, can get the whole BiGG database, VMH mapping file, MetaNetX translation file.
+3. Function to convert compartment suffixes.
+4. Import from a Excel file or csv file.
+5. ID match without special characters
---
+++ setup.py
@@ -0,0 +1,54 @@
+from os.path import abspath, dirname, join
+from sys import argv, path
+from setuptools import setup, find_packages
+
+# To temporarily modify sys.path
+SETUP_DIR = abspath(dirname(__file__))
+
+# Import version to get the version string
+path.insert(0, join(SETUP_DIR, 'cobrababel'))
+from version import get_version, update_release_version
+path.pop(0)
+version = get_version(pep440=True)
+
+# If building something for distribution, ensure the VERSION
+# file is up to date
+if 'sdist' in argv or 'bdist_wheel' in argv:
+    update_release_version()
+
+try:
+    with open('README.rst') as handle:
+        description = handle.read()
+except:
+    description = ''
+
+setup(
+    name='cobrababel',
+    version=version,
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=['cobra >= 0.5.6', 'six>=1.9.0', 'requests'],
+    tests_require=['pytest'],
+    package_data={},
+    author='Michael Mundy, Helena Mendes-Soares, Nicholas Chia',
+    author_email='mundy.michael@mayo.edu',
+    description='cobra babel',
+    long_description=description,
+    license='BSD',
+    keywords='metabolism biology optimization flux balance analysis fba',
+#    url='https://opencobra.github.io/cobrapy',
+    download_url='https://pypi.python.org/pypi/modelseed',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    platforms='GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7'
+)
