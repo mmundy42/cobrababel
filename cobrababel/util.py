f929afe8964264e4f5ae28e144fa4264c259c918
Mike Mundy <mundy.michael@mayo.edu>
2017-09-25 09:25:51 -0500

    Add get_models_in_folder() utility function

--- cobrababel/util.py
+++ cobrababel/util.py
@@ -18,3 +18,29 @@ def format_long_string(string, max_length):
         string = string[:max_length - 3]
         string += '...'
     return string
+
+
+def get_models_in_folder(source_folder):
+    """ Get a list of model file path names based on supported extensions.
+
+    Note that the contents of a file with one of the supported extensions might
+    not be a model so this should only be used for folders where all of the files
+    are model files.
+
+    Parameters
+    ----------
+    source_folder : str
+        Path to folder with source model files
+
+    Returns
+    -------
+    list of str
+        List of path names to source model files
+    """
+
+    source_models = list()
+    for filename in listdir(source_folder):
+        if filename.endswith('.mat') or filename.endswith('.xml') or \
+                filename.endswith('.sbml') or filename.endswith('.json'):
+            source_models.append(join(source_folder, filename))
+    return source_models
